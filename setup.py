c3ad314c0a4992d719187e808ca0acb81239370b
Francesca Capel <capel.francesca@gmail.com>
2019-10-22 10:27:38 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = 'icecube_tools',
+    packages = setuptools.find_packages(),
+    version = 'v0.1',
+    description = 'Tools for the analysis of ICeCube public data',
+    author = 'Francesca Capel',
+    author_email = 'capel.francesca@gmail.com',
+    url = 'https://github.com/cescalara/icecube_tools',
+    download_url = 'https://github.com/cescalara/icecube_tools/',
+    keywords = ['neutrino', 'analysis'],
+    classifiers = [],
+)
