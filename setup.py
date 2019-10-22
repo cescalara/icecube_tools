a4383f91e5586dd3d058b69877047b10ab0d4869
Francesca Capel <capel.francesca@gmail.com>
2019-10-22 11:51:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 setuptools.setup(
     name = 'icecube_tools',
     packages = setuptools.find_packages(),
-    version = 'v0.1',
-    description = 'Tools for the analysis of ICeCube public data',
+    version = 'v0.2',
+    description = 'Tools for the analysis of IceCube public data',
     author = 'Francesca Capel',
     author_email = 'capel.francesca@gmail.com',
     url = 'https://github.com/cescalara/icecube_tools',
