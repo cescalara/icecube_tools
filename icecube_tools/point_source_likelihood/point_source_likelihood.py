b39f0940ec167d0428bcac776bf647373a29d1fd
Francesca Capel <capel.francesca@gmail.com>
2019-11-14 14:01:19 +0100

    Add new SpatialOnlyPointSourceLikelihood

--- icecube_tools/point_source_likelihood/point_source_likelihood.py
+++ icecube_tools/point_source_likelihood/point_source_likelihood.py
@@ -23,6 +23,7 @@ class PointSourceLikelihood():
     Calculate the point source likelihood for a given 
     neutrino dataset - in terms of reconstructed 
     energies and arrival directions.
+    Based on what is described in Braun+2008.
     """    
     
     def __init__(self, direction_likelihood, energy_likelihood, event_coords, energies, source_coord):
@@ -322,6 +323,279 @@ class PointSourceLikelihood():
         
         return self.test_statistic
 
+
+
+class SpatialOnlyPointSourceLikelihood():
+    """
+    Calculate the point source likelihood for a given 
+    neutrino dataset - in terms of reconstructed 
+    arrival directions.
+
+    This class is exactly as in PointSourceLikelihood, 
+    but without the energy depedence.
+    """    
+    
+    def __init__(self, direction_likelihood, event_coords, source_coord):
+        """
+        Calculate the point source likelihood for a given 
+        neutrino dataset - in terms of reconstructed 
+        energies and arrival directions.
+        
+        :param direction_likelihood: An instance of SpatialGaussianLikelihood.
+        :param event_coords: List of (ra, dec) tuples for reconstructed coords.
+        :param source_coord: (ra, dec) pf the point to test.
+        """
+
+        self._direction_likelihood = direction_likelihood 
+  
+        self._band_width = 5 * self._direction_likelihood._sigma # degrees
+
+        self._band_solid_angle = ((2 * self._band_width) / 180) * 4 * np.pi
+        
+        self._event_coords = event_coords
+        
+        self._source_coord = source_coord
+
+        self._bg_index = 3.8
+
+        self._ns_min = 0.0
+        self._ns_max = 100
+        self._max_index = 3.8
+
+        self._select_nearby_events()
+
+        self.Ntot = len(self._event_coords)
+        
+
+    def _select_nearby_events(self):
+
+        ras = np.array([_[0] for _ in self._event_coords])
+
+        decs = np.array([_[1] for _ in self._event_coords])
+
+        source_ra, source_dec = self._source_coord
+
+        dec_fac = np.deg2rad(self._band_width)
+        
+        selected = list( set(np.where((decs >= source_dec - dec_fac) & (decs <= source_dec + dec_fac)
+                            & (ras >= source_ra - dec_fac) & (ras <= source_ra + dec_fac))[0]) )
+
+        selected_dec_band = np.where((decs >= source_dec - dec_fac) & (decs <= source_dec + dec_fac))[0]
+        
+        self._selected = selected
+        
+        self._selected_event_coords = [(ec[0], ec[1]) for ec in self._event_coords
+                                       if (ec[1] >= source_dec - dec_fac) & (ec[1] <= source_dec + dec_fac)
+                                       & (ec[0] >= source_ra - dec_fac) & (ec[0] <= source_ra + dec_fac)]
+        
+        self.Nprime = len(selected)
+
+        self.N = len(selected_dec_band)
+        
+        
+    def _signal_likelihood(self, event_coord, source_coord):
+
+        return self._direction_likelihood(event_coord, source_coord) 
+
+
+    def _background_likelihood(self):
+
+        return 1.0 / self._band_solid_angle
+ 
+        
+    def _get_neg_log_likelihood_ratio(self, ns):
+        """
+        Calculate the -log(likelihood_ratio) for minimization.
+
+        Uses calculation described in:
+        https://github.com/IceCubeOpenSource/SkyLLH/blob/master/doc/user_manual.pdf
+
+        :param ns: Number of source counts.
+        """
+        
+        one_plus_alpha = 1e-10 
+        alpha = one_plus_alpha - 1
+        
+        log_likelihood_ratio = 0.0
+        
+        for i in range(self.Nprime):
+            
+            signal = self._signal_likelihood(self._selected_event_coords[i], self._source_coord)
+
+            bg = self._background_likelihood()
+
+            chi = (1 / self.N) * (signal/bg - 1)
+
+            alpha_i = ns * chi
+               
+            if (1 + alpha_i) < one_plus_alpha:
+
+                alpha_tilde = (alpha_i - alpha) / one_plus_alpha 
+                log_likelihood_ratio += np.log1p(alpha) + alpha_tilde - (0.5 * alpha_tilde**2) 
+
+            else:
+                
+                log_likelihood_ratio += np.log1p(alpha_i)
+
+        log_likelihood_ratio += (self.N - self.Nprime) * np.log1p(-ns / self.N)
+            
+        return -log_likelihood_ratio
+
+    
+    def _get_log_likelihood(self, ns=0.0):
+        """
+        Calculate -log(likelihood) where likelihood is the 
+        full point source likelihood. Negative is reutrned for
+        easy minimization.
+
+        Evaluated at the best fit ns and index, this is the 
+        maximum likelihood for the source + background hypothesis.
+        Evaluated at ns=0, index=None, this is the likelihood 
+        for the background only hypothesis.
+
+        :param ns: Number of source counts.
+        :param index: Spectral index of source.
+        """
+
+        log_likelihood = 0.0
+
+        for i in range(self.N):
+
+            if index:
+                
+                signal = self._signal_likelihood(self._selected_event_coords[i], self._source_coord)
+                S_i = (ns / self.N) * signal
+
+            else:
+
+                S_i = 0
+                
+            bg = self._background_likelihood()
+
+            B_i = (1 - ns/self.N) * bg
+            
+            log_likelihood += np.log(S_i + B_i)
+
+        return -log_likelihood
+
+    
+    def __call__(self, ns, index):
+        """
+        Wrapper function for convenience.
+        """
+
+        return self._get_neg_log_likelihood_ratio(ns, index)
+
+    
+    def _minimize(self):
+        """
+        Minimize -log(likelihood_ratio) for the source hypothesis, 
+        returning the best fit ns and index.
+
+        Uses the iMiuint wrapper.
+        """
+
+
+        init_ns = self._ns_min + (self._ns_max - self._ns_min)/2 
+
+        m = Minuit(self._get_neg_log_likelihood_ratio, ns=init_ns,
+                   error_ns=0.1, errordef=0.5,
+                   limit_ns=(self._ns_min, self._ns_max))
+        m.migrad()
+        
+        self._best_fit_ns = m.values['ns']
+
+        
+    def _minimize_grid(self):
+        """
+        Minimize -log(likelihood_ratio) for the source hypothesis, 
+        returning the best fit ns and index.
+        
+        This simple grid method takes roughly the same time as minuit 
+        and is more accurate... 
+        """
+
+        ns_grid = np.linspace(self._ns_min, self._ns_max, 10)
+
+        out = np.zeros(len(ns_grid))
+        for i, ns in enumerate(ns_grid):
+                out[i] = self._get_neg_log_likelihood_ratio(ns)
+
+        sel = np.where(out==np.min(out))
+
+        if len(sel[0]) > 1:
+
+            self._best_fit_ns = 0.0
+
+        else:
+
+            self._best_fit_ns = ns_grid[sel[0]]
+
+    
+    def _first_derivative_likelihood_ratio(self, ns=0):
+        """
+        First derivative of the likelihood ratio. 
+        Equation 41 in
+        https://github.com/IceCubeOpenSource/SkyLLH/blob/master/doc/user_manual.pdf.  
+        """
+
+        one_plus_alpha = 1e-10
+        alpha = one_plus_alpha - 1
+        
+        self._first_derivative = []
+        
+        for i in range(self.Nprime):
+
+            signal = self._signal_likelihood(self._selected_event_coords[i], self._source_coord) 
+
+            bg = self._background_likelihood()
+            
+            chi_i = (1 / self.N) * ((signal/bg) - 1)
+
+            alpha_i = ns * chi_i
+               
+            if (1 + alpha_i) < one_plus_alpha:
+
+                alpha_tilde = (alpha_i - alpha) / one_plus_alpha
+            
+                self._first_derivative.append( (1 / one_plus_alpha) * (1 - alpha_tilde) * chi_i )
+
+            else:
+
+                self._first_derivative.append( chi_i / (1 + alpha_i) )
+
+        self._first_derivative = np.array(self._first_derivative)
+                
+        return sum(self._first_derivative) - ((self.N  - self.Nprime) / (self.N - ns))
+
+
+    def _second_derivative_likelihood_ratio(self, ns=0):
+        """
+        Second derivative of the likelihood ratio.
+        Equation 44 in
+        https://github.com/IceCubeOpenSource/SkyLLH/blob/master/doc/user_manual.pdf.
+        """
+
+        self._second_derivative = -(self._first_derivative)**2 
+            
+        return sum(self._second_derivative) - ((self.N - self.Nprime) / (self.N - ns)**2)
+        
+            
+    def get_test_statistic(self):
+        """
+        Calculate the test statistic for the best fit ns
+        """
+
+        self._minimize()
+
+        neg_log_lik = self._get_neg_log_likelihood_ratio(self._best_fit_ns)
+        
+        self.likelihood_ratio = np.exp(neg_log_lik)
+        
+        self.test_statistic = -2 * neg_log_lik
+        
+        return self.test_statistic
+
     
 
 class SimplePointSourceLikelihood():
@@ -330,6 +604,7 @@ class SimplePointSourceLikelihood():
     def __init__(self, direction_likelihood, event_coords, source_coord):
         """
         Point source likelihood with only spatial information.
+        Testing out simple algorithms for evaluating TS.
         """
 
         self._direction_likelihood = direction_likelihood
@@ -396,6 +671,8 @@ class SimpleWithEnergyPointSourceLikelihood():
     def __init__(self, direction_likelihood, energy_likelihood, event_coords, source_coord):
         """
         Simple version of point source likelihood.
+        Also including the energy dependence.
+        Testing out simple algorithms for evaluating TS.  
         """
 
         self._direction_likelihood = direction_likelihood
