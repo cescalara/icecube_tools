c93543a3ae0df335e7ba692cb6dc2c9d796aaf2e
Francesca Capel <capel.francesca@gmail.com>
2021-12-29 16:11:40 +0100

    Add API docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = "icecube_tools"
@@ -24,13 +23,17 @@ author = "Francesca Capel"
 # The full version, including alpha/beta/rc tags
 release = "v3.2"
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["nbsphinx", "sphinx.ext.githubpages"]
+extensions = [
+    "nbsphinx",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -47,8 +50,11 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+html_theme_options = {"display_version": True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+autosummary_generate = False
--- docs/index.rst
+++ docs/index.rst
@@ -6,9 +6,14 @@
 Welcome to icecube_tools's documentation!
 =========================================
 
+The ``icecube_tools`` package is designed to make it easier to interact with the publicly available information provided by the `IceCube Neutrino Observatory <https://icecube.wisc.edu>`_ in Python. You can use it to download datasets as well as load detector information like effective areas, energy resolution and angular resolution in a unified format. This information can then be brought together with simple astrophysical source models to run simulations of neutrino detections, or even to evaluate the likelihood and reproduce the results of the standard IceCube point source searches. 
+
+You can find more information on how to make use of ``icecube_tools`` in the example notebooks listed below. The source code for these notebooks can be found in markdown format `here <https://github.com/cescalara/icecube_tools/docs/markdown>`_ in the GitHub repository. These markdown files can then be converted to ``ipynb`` format by using ``jupytext``.
+
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
    notebooks/public_data_access.ipynb
    notebooks/using_public_info.ipynb
+   api/api
