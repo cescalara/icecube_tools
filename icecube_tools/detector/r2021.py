fixed matrix-like calculation of scalar products

icecube_tools/detector/r2021.py
@@ -6,6 +6,7 @@ from astropy import units as u
 from vMF import sample_vMF
 
 import logging
+logging.basicConfig(level=logging.INFO)
 
 from icecube_tools.detector.energy_resolution import EnergyResolution
 from icecube_tools.detector.angular_resolution import AngularResolution
@@ -64,7 +65,7 @@ class R2021IRF(EnergyResolution, AngularResolution):
         :return: Angle between vectors in degrees
         """
 
-        return np.rad2deg(np.arccos(np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))))
+        return np.rad2deg(np.arccos(np.dot(vec1.T, vec2) / (np.linalg.norm(vec1, axis=0) * np.linalg.norm(vec2, axis=0))))
 
 
     def sample_energy(self, coord, Etrue, seed=42):
@@ -127,7 +128,7 @@ class R2021IRF(EnergyResolution, AngularResolution):
         ra, dec = coord
         sky_coord = SkyCoord(ra=ra * u.rad, dec=dec * u.rad, frame="icrs")
         sky_coord.representation_type = "cartesian"
-        unit_vector = np.array([sky_coord.x, sky_coord.y, sky_coord.z]).T
+        unit_vector = np.array([sky_coord.x, sky_coord.y, sky_coord.z])
 
         if isinstance(Etrue, np.ndarray):
             size = Etrue.size
@@ -204,22 +205,24 @@ class R2021IRF(EnergyResolution, AngularResolution):
                             ang_err[_index_k] = self.marginal_pdf_angerr(idx_e, idx_d, idx_e_r, idx_k, 'pdf').rvs(size=_index_k[0].size, random_state=seed)
 
         #kappa needs an angle in degrees, prob of containment, here 0.5 as stated in the paper
-        for c, ang in enumerate(np.power(10, ang_err)):
-            kappa = get_kappa(ang, 0.5)
-            new_unit_vector = sample_vMF(unit_vector[c], kappa, 1)[0]
+        # for c, ang in enumerate(np.power(10, ang_err)):
+        kappa = get_kappa(ang_err, 0.5)
+        logging.info(kappa.shape)
+        logging.info(unit_vector.shape)
+        new_unit_vector = sample_vMF(unit_vector, kappa)
 
-            #create sky coordinates from rotated/deflected vector
-            new_sky_coord = SkyCoord(
-                x=new_unit_vector[0],
-                y=new_unit_vector[1],
-                z=new_unit_vector[2],
-                representation_type="cartesian",
-            )
-            new_sky_coord.representation_type = "unitspherical"
-            new_ras[c] = new_sky_coord.ra.rad
-            new_decs[c] = new_sky_coord.dec.rad
-            reco_ang_err[c] = self.get_angle(new_unit_vector, unit_vector[c]) 
-
+        #create sky coordinates from rotated/deflected vector
+        new_sky_coord = SkyCoord(
+            x=new_unit_vector[0],
+            y=new_unit_vector[1],
+            z=new_unit_vector[2],
+            representation_type="cartesian",
+        )
+        new_sky_coord.representation_type = "unitspherical"
+        new_ras = new_sky_coord.ra.rad
+        new_decs = new_sky_coord.dec.rad
+        reco_ang_err = np.diag(self.get_angle(new_unit_vector, unit_vector))
+        logging.info(f"reco_ang_error shape: {reco_ang_err.shape}")
         return new_ras, new_decs, reco_ang_err, np.power(10, Ereco)
 
 

icecube_tools/simulator.py
@@ -4,9 +4,10 @@ from astropy import units as u
 import h5py
 from scipy.stats import uniform
 import logging
-logging.basicConfig(level=logging.CRITICAL)
+logging.basicConfig(level=logging.INFO)
 
 # from memory_profiler import profile
+from tqdm import tqdm as progress_bar
 
 from .detector.detector import Detector
 from .source.source_model import Source, DIFFUSE, POINT
@@ -138,22 +139,22 @@ class Simulator:
         Earr_d = {i: np.zeros(l_num[i]) for i in l_set}
         
         accepted = np.zeros(self.N, dtype=bool)
-        
+ 
         #go over each source
         for i in l_set:
             #simulate until appropriate number of events is accepted
-            
+
             max_energy[i] = self.sources[i].flux_model._upper_energy
-            
+
             logging.info(f"source: {i}")
-            
+
             while True:
                 #check if data is needed, else break loop
                 where_zero = np.argwhere(Etrue_d[i] == 0.)
                 if where_zero.size == 0:
                     logging.debug("no more empty slots, done")
                     break
-                
+
                 Etrue_ = self.sources[i].flux_model.sample(num)
                 if self.sources[i].source_type == DIFFUSE:
 
@@ -225,6 +226,7 @@ class Simulator:
         then the number from each source will be weighted
         accordingly.
         :param N: Set expected number of neutrinos manually.
+        :param seed: Set random seed.
         """
 
         np.random.seed(seed)
@@ -321,8 +323,10 @@ class Simulator:
                         ra_d[i][start:] = ra_[idx][0:remaining]
                         dec_d[i][start:] = dec_[idx][0:remaining]
                         break
-            # print("Sampling spectrum is done") 
+            # print("Sampling spectrum is done")
+            logging.info("Done sampling the spectrum") 
             if not isinstance(self.detector.energy_resolution, R2021IRF):
+                logging.info("Sampled reco energy")
                 Ereco = self.detector.energy_resolution.sample(Earr_d[i])
                 #this and all following try-except TypeError blocks
                 #are needed if only a single event is sampled
@@ -343,7 +347,8 @@ class Simulator:
                     try:
                         self.ang_err += list(reco_ang_err)
                         self.reco_energy += list(Ereco)
-                    except TypeError:
+                    except TypeError as e:
+                        print(e)
                         self.ang_err += [reco_ang_err]
                         self.reco_energy += [Ereco]
                         
@@ -367,6 +372,7 @@ class Simulator:
                 except TypeError:
                     self.dec += [dec_d[i]]
                     self.ra += [ra_d[i]]
+                logging.info("Sampled angular uncertainty for diffuse source")
             else:
 
 
@@ -393,15 +399,20 @@ class Simulator:
                     self.ang_err +=list(reco_ang_err)
                     self.dec += list(reco_dec)
                     self.ra += list(reco_ra)
-                except TypeError:
+                except TypeError as e:
+                    print(e)
                     self.ang_err += [reco_ang_err]
                     self.dec += [reco_dec]
-                    self.ra += [reco_ra] 
+                    self.ra += [reco_ra]
+                logging.info("Sampled angular uncertainty for point source")
 
         self.true_energy = np.concatenate(tuple(Etrue_d[k] for k in Earr_d.keys()))
         self.arrival_energy = np.concatenate(tuple(Earr_d[k] for k in Earr_d.keys()))
         self.source_label = np.concatenate(tuple(np.full(l_num[l], l, dtype=int) for l in Earr_d.keys()))
-        
+        logging.info("Created array of simulation data")       
+
+
+ 
     def save(self, filename):
         """
         Save the output to filename.